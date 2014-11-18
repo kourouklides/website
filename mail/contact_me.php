ce7715e99052b9fe0777434b64ac5148790c2696
Ioannis Kourouklides <leonariso@users.noreply.github.com>
2014-11-18 02:59:02 +0200

    Update contact_me.php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -19,7 +19,7 @@ $message = $_POST['message'];
 $to = 'mike@broskico.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Frenzi website enquiry from:  $email_address";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nSubject: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@broskico.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: mike@broskico.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
